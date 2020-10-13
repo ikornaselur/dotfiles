2eca969b64394235eddfccc19d491d734a44a12e
Axel Örn <dev@absalon.is>
2020-10-13 14:22:02 +0100

    Update python upgrade script to use 3.9

--- python/update-python.sh
+++ python/update-python.sh
@@ -9,12 +9,11 @@ echo -e "${GREEN}[+] Upgrading pyenv...${NC}"
 brew upgrade pyenv
 
 VERSION27=`pyenv install --list | grep -E "^\s+2.7.\d+$" | tail -1 | tr -d '[:space:]'`
-VERSION35=`pyenv install --list | grep -E "^\s+3.5.\d+$" | tail -1 | tr -d '[:space:]'`
-VERSION36=`pyenv install --list | grep -E "^\s+3.6.\d+$" | tail -1 | tr -d '[:space:]'`
 VERSION37=`pyenv install --list | grep -E "^\s+3.7.\d+$" | tail -1 | tr -d '[:space:]'`
 VERSION38=`pyenv install --list | grep -E "^\s+3.8.\d+$" | tail -1 | tr -d '[:space:]'`
+VERSION39=`pyenv install --list | grep -E "^\s+3.9.\d+$" | tail -1 | tr -d '[:space:]'`
 
-VERSIONS="${VERSION38} ${VERSION37} ${VERSION27} ${VERSION36} ${VERSION35}"
+VERSIONS="${VERSION38} ${VERSION27} ${VERSION39} ${VERSION37}"
 
 echo -e "${GREEN}[+] Will install ${VERSIONS}${NC}"
 echo -ne "${ORANGE}"
@@ -23,16 +22,14 @@ echo -e "${NC}"
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo -e "${GREEN}[+] Installing ${VERSION27} [1/5]...${NC}"
+  echo -e "${GREEN}[+] Installing ${VERSION27} [1/4]...${NC}"
   pyenv install ${VERSION27}
-  echo -e "${GREEN}[+] Installing ${VERSION35} [2/5]...${NC}"
-  pyenv install ${VERSION35}
-  echo -e "${GREEN}[+] Installing ${VERSION36} [3/5]...${NC}"
-  pyenv install ${VERSION36}
-  echo -e "${GREEN}[+] Installing ${VERSION37} [4/5]...${NC}"
+  echo -e "${GREEN}[+] Installing ${VERSION37} [2/4]...${NC}"
   pyenv install ${VERSION37}
-  echo -e "${GREEN}[+] Installing ${VERSION38} [5/5]...${NC}"
+  echo -e "${GREEN}[+] Installing ${VERSION38} [3/4]...${NC}"
   pyenv install ${VERSION38}
+  echo -e "${GREEN}[+] Installing ${VERSION39} [4/4]...${NC}"
+  pyenv install ${VERSION39}
   echo -e "${GREEN}[+] Setting global versions to ${VERSIONS}...${NC}"
   pyenv global ${VERSIONS}
 
