1db14721af0e6ebcbc21969947624a5393770865
Axel Örn <dev@absalon.is>
2021-06-08 21:07:38 +0100

    Set up ZSH and oh-my-zsh

--- macos/init.sh
+++ macos/init.sh
@@ -9,17 +9,23 @@ sudo -k  #  Reset sudo timestamp
 
 # Install common apps/utils from homebrew
 brew install \
+  autojump \
+  direnv \
   exa \
   ffmpeg \
   git \
   htop \
+  hub \
   imagemagick \
   neovim \
   pyenv \
+  ripgrep \
   tig \
   tmux \
-  wget
+  wget \
+  zsh
 
+brew tap homebrew/cask-fonts
 brew install --cask \
   alacritty \
   google-chrome \
@@ -45,19 +51,46 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Cargo install apps
 ~/.cargo/bin/cargo install \
-  cargo-edit
+  cargo-edit \
+  litime
 
 # Create folders
 mkdir -p ~/.config/alacritty
 mkdir -p ~/Projects
 git clone https://github.com/ikornaselur/dotfiles.git ~/Projects/dotfiles
 
-# Hook up dot files
+# Hook up dotfiles
 ln -s ~/Projects/dotfiles/macos/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-# Configure tmux
+# Install Fira Code font
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Retina/complete/Fira%20Code%20Retina%20Nerd%20Font%20Complete.ttf -O "/Library/Fonts/Fira Code.ttf"
 
-# Set up ZSH, oh-my-zsh and powerlevel10k
+# Configure tmux
+ln -s ~/Projects/dotfiles/tmux.conf ~/.tmux.conf
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
+
+# Set up ZSH and oh-my-zsh
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
+rm ~/.zshrc
+ln -s ~/Projects/dotfiles/macos/zshrc ~/.zshrc
+touch ~/.zshrc.extra
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+compaudit | xargs chmod g-w,o-w
+
+# Set up powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+
+# Link the powerlevel10k config
+ln -s ~/Projects/dotfiles/macos/p10k.zsh ~/.p10k.zsh
+
+# Set default shell
+chsh -s zsh
+
+# Set up neovim
 
 # Set up Python + Poetry
 
