ebc08608c1bcad9505b6e994ae7243bfc329fbb4
Axel Örn Sigurðsson <dev@absalon.is>
2021-06-16 00:53:44 +0100

    Configure gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,9 +1,11 @@
 require('gitsigns').setup({
-    signs = {
-        add = { hl = 'GitGutterAdd', text = '+' },
-        change = { hl = 'GitGutterChange', text = '~' },
-        delete = { hl = 'GitGutterDelete', text = '_' },
-        topdelete = { hl = 'GitGutterDelete', text = '‾' },
-        changedelete = { hl = 'GitGutterChange', text = '~' },
-    },
+  keymaps = {
+    noremap = true,
+    buffer = true,
+    ['n gp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n gb'] = '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>',
+  },
+  current_line_blame_delay=0,
+  use_decoration_api = true,
+  use_internal_diff = true,
 })
