a44028ea9c55d95a549602a098a6be1725052e4a
Axel Örn Sigurðsson <dev@absalon.is>
2021-06-16 13:34:32 +0100

    Set up neovim nightly and Paq in MacOS init

--- macos/init.sh
+++ macos/init.sh
@@ -22,7 +22,6 @@ brew install \
   imagemagick \
   jq \
   mosh \
-  neovim \
   nodejs \
   pyenv \
   ripgrep \
@@ -31,6 +30,10 @@ brew install \
   tmux \
   wget \
   zsh
+
+# Install nightly neovim for v0.5
+brew install --head luajit
+brew install --head neovim
 
 brew tap homebrew/cask-fonts
 brew install --cask \
@@ -142,8 +145,8 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/cu
 ln -s ~/Projects/dotfiles/macos/p10k.zsh ~/.p10k.zsh
 
 # Set up neovim
-ln -s ~/Projects/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -s ~/Projects/dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim \
-  --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +'PlugInstall --sync' +qall
+ln -s ~/Projects/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+ln -s ~/Projects/dotfiles/nvim/lua ~/.config/nvim/lua
+git clone --depth=1 https://github.com/savq/paq-nvim.git \
+    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
+nvim +PaqInstall
