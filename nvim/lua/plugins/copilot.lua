f6892cccf5876cfb17132b12ca80bcb0f935fbb7
Axel Örn Sigurðsson <dev@absalon.is>
2023-04-07 12:46:12 +0100

    Opt-in filetypes for copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,5 +1,13 @@
 require("copilot").setup({
   suggestion = { enabled = false },
   panel = { enabled = false },
+  filetypes = {
+    python = true,
+    rust = true,
+    lua = true,
+    javascript = true,
+    typescript = true,
+    ["*"] = false,
+  },
 })
 require("copilot_cmp").setup()
