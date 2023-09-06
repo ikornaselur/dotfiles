6b513112bf9d6e7df2ee6a36775a857d0df395d4
Axel Orn Sigurdsson <axel@senseon.io>
2023-09-06 18:45:03 +0100

    Refactor hammerspoon config for recent version

--- macos/hammerspoon.init.lua
+++ macos/hammerspoon.init.lua
@@ -1,39 +1,36 @@
--- https://github.com/asmagill/hs._asm.undocumented.spaces
-local spaces = require('hs._asm.undocumented.spaces')
+local spaces = require('hs.spaces')
 
 -- Switch alacritty
 hs.hotkey.bind({'cmd'}, '§', function ()
   local APP_NAME = 'Alacritty'
-  function moveWindow(alacritty, space, mainScreen)
-    -- move to main space
+  function moveWindow(alacritty, mainScreen)
     local win = nil
     while win == nil do
       win = alacritty:mainWindow()
     end
     winFrame = win:frame()
     win:setFrame(winFrame, 0)
-    win:spacesMoveTo(space)
+    win:moveToScreen(mainScreen)
     win:focus()
   end
   local alacritty = hs.application.get(APP_NAME)
   if alacritty ~= nil and alacritty:isFrontmost() then
     alacritty:hide()
   else
-    local space = spaces.activeSpace()
-    local mainScreen = hs.screen.find(spaces.mainScreenUUID())
+    local mainScreen = hs.screen.mainScreen()
     if alacritty == nil and hs.application.launchOrFocus(APP_NAME) then
       local appWatcher = nil
       appWatcher = hs.application.watcher.new(function(name, event, app)
         if event == hs.application.watcher.launched and name == APP_NAME then
           app:hide()
-          moveWindow(app, space, mainScreen)
+          moveWindow(app, mainScreen)
           appWatcher:stop()
         end
       end)
       appWatcher:start()
     end
     if alacritty ~= nil then
-      moveWindow(alacritty, space, mainScreen)
+      moveWindow(alacritty, mainScreen)
     end
   end
 end)
