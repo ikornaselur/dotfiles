5b90302e1f461777368d80cc4fd8cff0fc025943
Axel Orn Sigurdsson <axel@senseon.io>
2024-11-07 11:45:10 +0100

    Tweak hammerspoon script for multiple buttons

--- hammerspoon/hammerspoon.init.lua
+++ hammerspoon/hammerspoon.init.lua
@@ -1,23 +1,22 @@
 local spaces = require('hs.spaces')
 
--- Switch to WezTerm
-hs.hotkey.bind({'cmd'}, '§', function ()
-  local APP_NAME = 'WezTerm'
+-- Function to switch to app
+function switchToApp(app_name)
   function moveWindow(wezterm, mainScreen)
     local win = wezterm:mainWindow()
     local space = hs.spaces.activeSpaceOnScreen()
     hs.spaces.moveWindowToSpace(win:id(), space)
     win:focus()
   end
-  local wezterm = hs.application.get(APP_NAME)
+  local wezterm = hs.application.get(app_name)
   if wezterm ~= nil and wezterm:isFrontmost() then
     wezterm:hide()
   else
     local mainScreen = hs.screen.mainScreen()
-    if wezterm == nil and hs.application.launchOrFocus(APP_NAME) then
+    if wezterm == nil and hs.application.launchOrFocus(app_name) then
       local appWatcher = nil
       appWatcher = hs.application.watcher.new(function(name, event, app)
-        if event == hs.application.watcher.launched and name == APP_NAME then
+        if event == hs.application.watcher.launched and name == app_name then
           app:hide()
           moveWindow(app, mainScreen)
           appWatcher:stop()
@@ -29,4 +28,15 @@ hs.hotkey.bind({'cmd'}, '§', function ()
       moveWindow(wezterm, mainScreen)
     end
   end
+end
+
+
+
+-- Switch to WezTerm
+hs.hotkey.bind({'cmd'}, '`', function ()
+  switchToApp('WezTerm')
+end)
+
+hs.hotkey.bind({'cmd'}, '§', function ()
+  switchToApp('WezTerm')
 end)
