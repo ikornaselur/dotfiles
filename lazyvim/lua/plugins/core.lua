70df4a17ebe67845620df0592d6debba1681247a
Axel Örn Sigurðsson <git@absalon.dev>
2025-07-20 11:53:33 +0200

    Add LazyVim config

---
+++ lazyvim/lua/plugins/core.lua
@@ -0,0 +1,31 @@
+return {
+  { "sainnhe/gruvbox-material" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+
+  {
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+  },
+
+  { "folke/flash.nvim", enabled = false },
+
+  {
+    "smoka7/hop.nvim",
+    version = "*",
+    opts = {
+      keys = "etovxqpdygfblzhckisuran",
+    },
+    config = function(_, opts)
+      require("hop").setup(opts)
+      vim.keymap.set("n", "f", "<cmd>HopWord<CR>", { desc = "Hop to word" })
+    end,
+  },
+}
