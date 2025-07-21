137655cdedc6525c689f68ee9a99166b291158fc
Axel Örn Sigurðsson <git@absalon.dev>
2025-07-21 11:02:18 +0200

    Add diagnostic keybindings in LazyVim

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- -- Go to previous diagnostic
+vim.keymap.set("n", "<C-h>", vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
+
+-- Go to next diagnostic
+vim.keymap.set("n", "<C-l>", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
