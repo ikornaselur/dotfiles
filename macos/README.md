134f69ddc5cf4dd68f64493dc234c9a1aaa21ec0
Axel Örn Sigurðsson <git@absalon.dev>
2025-07-21 11:23:40 +0200

    Update macos readme

--- macos/README.md
+++ macos/README.md
@@ -1,8 +1,12 @@
-# My macos dotfiles
+# My MacOS dotfiles
 
-`init.sh` will bootstrap a fresh setup of macos, installing different tools and
+`bootstrap.sh` will bootstrap a fresh setup of MacOS, installing different tools and
 apps that I use.
 
 To run on a fresh install, open Terminal and run
 
-curl https://raw.githubusercontent.com/ikornaselur/dotfiles/master/macos/init.sh | sh
+```bash
+curl https://raw.githubusercontent.com/ikornaselur/dotfiles/main/macos/bootstrap.sh | sh
+```
+
+Note.. I'm not expecting anyone else to use this, this is just a quick way for me to get up and running with a new machine
--- macos/bootstrap.sh
+++ macos/bootstrap.sh
@@ -10,14 +10,14 @@ read -p "Press any key to continue after xcode tools finish installing"
 echo "Requesting sudo permission for installing homebrew"
 sudo echo ""
 CI=true /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-sudo -k  #  Reset sudo timestamp
+sudo -k #  Reset sudo timestamp
 
 # Set up ansible with brew
 brew install ansible
 ansible-galaxy collection install community.general
 
-# Clone the repo 
-git clone https://github.com/ikornaselur/dotfiles.git --branch ans ~/.dotfiles 
+# Clone the repo
+git clone https://github.com/ikornaselur/dotfiles.git ~/.dotfiles
 
 cd ~/.dotfiles/macos/ansible
 
