502cd6fbe4e7f4463d32a6e01e91ad3f36f258e8
Axel Örn Sigurðsson <git@absalon.dev>
2025-07-24 10:38:42 +0200

    Split out colorscheme config from core.lua

--- lazyvim/lua/plugins/core.lua
+++ lazyvim/lua/plugins/core.lua
@@ -1,22 +1,11 @@
 return {
-  { "sainnhe/gruvbox-material" },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox-material",
-    },
-  },
-
   {
     "snacks.nvim",
     opts = {
       scroll = { enabled = false },
     },
   },
-
   { "folke/flash.nvim", enabled = false },
-
   {
     "smoka7/hop.nvim",
     version = "*",
@@ -26,27 +15,6 @@ return {
     config = function(_, opts)
       require("hop").setup(opts)
       vim.keymap.set("n", "f", "<cmd>HopWord<CR>", { desc = "Hop to word" })
-    end,
-  },
-
-  {
-    "mg979/vim-visual-multi",
-    branch = "master",
-    lazy = false,
-    init = function()
-      vim.g.VM_default_mappings = 0
-      vim.g.VM_maps = {
-        ["Add Cursor Down"] = "<C-J>",
-        ["Add Cursor Up"] = "<C-K>",
-      }
-      vim.g.VM_set_statusline = 0
-    end,
-  },
-  {
-    "f-person/auto-dark-mode.nvim",
-    lazy = false,
-    config = function()
-      require("auto-dark-mode").setup()
     end,
   },
 }
