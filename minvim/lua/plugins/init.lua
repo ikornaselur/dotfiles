a51ffa413b1ffbee2beaed44455bacd853176ec7
Axel Örn Sigurðsson <git@absalon.dev>
2025-12-07 12:59:29 +0100

    Remove reduntant plugins router

--- minvim/lua/plugins/init.lua
+++
@@ -1,7 +0,0 @@
--- Central list of plugin module imports. Lazy will pull in each file referenced here.
-return {
-  { import = "plugins.core" },
-  { import = "plugins.tooling" },
-  { import = "plugins.languages" },
-  -- Additional groups get appended here (completion, lsp, languages, etc.).
-}
