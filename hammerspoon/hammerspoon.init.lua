dc00b8de56bb56bdc4d62bc7da3eb6f3a7c88255
Axel Örn Sigurðsson <git@absalon.dev>
2026-03-20 10:52:37 +0100

    Slim down hte hammerspoon config since Apple removed the private APIs

--- hammerspoon/hammerspoon.init.lua
+++ hammerspoon/hammerspoon.init.lua
@@ -1,42 +1,21 @@
-local spaces = require('hs.spaces')
-
--- Function to switch to app
+-- Switch to app, or hide it if already focused.
+-- NOTE: Moving windows across spaces is broken since macOS Sequoia.
+-- Apple NOP'd the private APIs that hs.spaces.moveWindowToSpace relied on.
+-- See: https://github.com/Hammerspoon/hammerspoon/issues/3698
 function switchToApp(app_name)
-  function moveWindow(wezterm, mainScreen)
-    local win = wezterm:mainWindow()
-    local space = hs.spaces.activeSpaceOnScreen()
-    hs.spaces.moveWindowToSpace(win:id(), space)
-    win:focus()
-  end
-  local wezterm = hs.application.get(app_name)
-  if wezterm ~= nil and wezterm:isFrontmost() then
-    wezterm:hide()
-  else
-    local mainScreen = hs.screen.mainScreen()
-    if wezterm == nil and hs.application.launchOrFocus(app_name) then
-      local appWatcher = nil
-      appWatcher = hs.application.watcher.new(function(name, event, app)
-        if event == hs.application.watcher.launched and name == app_name then
-          app:hide()
-          moveWindow(app, mainScreen)
-          appWatcher:stop()
-        end
-      end)
-      appWatcher:start()
-    end
-    if wezterm ~= nil then
-      moveWindow(wezterm, mainScreen)
-    end
-  end
+	local app = hs.application.get(app_name)
+	if app ~= nil and app:isFrontmost() then
+		app:hide()
+	else
+		hs.application.launchOrFocus(app_name)
+	end
 end
 
-
-
 -- Switch to WezTerm
-hs.hotkey.bind({'cmd'}, '`', function ()
-  switchToApp('WezTerm')
+hs.hotkey.bind({ "cmd" }, "`", function()
+	switchToApp("WezTerm")
 end)
 
-hs.hotkey.bind({'cmd'}, '§', function ()
-  switchToApp('WezTerm')
+hs.hotkey.bind({ "cmd" }, "§", function()
+	switchToApp("WezTerm")
 end)
