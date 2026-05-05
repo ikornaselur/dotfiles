f6330b37292f43f5db188e25c605b866c4f49cb9
Axel Örn Sigurðsson <git@absalon.dev>
2026-05-05 17:26:09 +0200

    Yank over ssh

--- minvim/lua/config/clipboard.lua
+++ minvim/lua/config/clipboard.lua
@@ -1,6 +1,15 @@
 local M = {}
 
 function M.setup()
+  if vim.env.SSH_TTY then
+    local osc52 = require("vim.ui.clipboard.osc52")
+    vim.g.clipboard = {
+      name = "OSC 52",
+      copy = { ["+"] = osc52.copy("+"), ["*"] = osc52.copy("*") },
+      paste = { ["+"] = osc52.paste("+"), ["*"] = osc52.paste("*") },
+    }
+  end
+
   -- Try to ensure the system clipboard integration is available.
   -- If `unnamedplus` is unsupported, fall back to `unnamed` so yank/paste still work.
   if vim.fn.has("unnamedplus") == 1 then
